f70dedbd48e9877625bc6b8a325b424f26e02854
AkshayKannur97 <akshay@zincoelectric.com>
2022-12-27 10:46:34 +0530

    v0.2.1

--- db_utils.py
+++ db_utils.py
@@ -54,4 +54,4 @@ if __name__ == '__main__':
     # update_calibration_for_channel('ch12', decimals=34, resolution=45)
     # fetch_calibration_for_channel('ch12')
     # fetch_calibration_for_channel('ch2')
-    update_test_paths('002', pdf_path='/tmp/098475364.png')
+    update_test_paths('003', pdf_path='/tmp/098475364.png')
